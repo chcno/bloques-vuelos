58b8d21af5154df292c34d758ca939e2bbf2c9fa
Charlie Cano <charliecno04@gmail.com>
2025-07-18 12:07:40 -0500

    Intentando arreglar lo del correo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+
 config.action_mailer.default_url_options = { host: 'bloques.magflighttraining.com/', protocol: 'https' }
 
 config.action_mailer.smtp_settings = {
@@ -71,7 +73,7 @@ config.action_mailer.smtp_settings = {
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  #config.action_mailer.default_url_options = { host: "example.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
